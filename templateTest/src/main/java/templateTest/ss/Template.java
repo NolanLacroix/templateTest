8536f04d06f9349b978149e7123cd56eb47f2c95
Nolan <Nolan@DESKTOP-RE6V5IC.home>
2020-04-06 16:53:42 +0200

    #test

---
+++ templateTest/src/main/java/templateTest/ss/Template.java
@@ -0,0 +1,17 @@
+package templateTest.ss;
+
+import aa.ss.InitWebDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class Template 
+{
+    public void appelTest() {
+    	
+    	InitWebDriver.startWebDriver();
+    	
+    	System.out.println("template appelé");
+    }
+}
